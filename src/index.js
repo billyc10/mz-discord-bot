0c378a0b15b40f09c4b9b7317b73dc4d0dd62dbf
Billy Chen <billychen10@hotmail.com>
2020-09-18 20:38:00 +1000

    Jeffy bottttt

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log('hi');
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+const {users, token} = require('./config.json');
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+    if (msg.author == users.self) return;
+
+    if (msg.author == users.jeffrey) {
+        msg.channel.send(`Woah woah hold up there. The big man <@!${users.jeffrey}> has graced us with his present? What a legend. What a brave brave man. Thank you.`)
+    }
+
+    const message = msg.content.toLowerCase();
+    if (message.includes('valorant') || message.includes('val')) {
+        msg.channel.send(`Valorant? Do we need one more? One more brave man such as <@!${users.jeffrey}> perhaps!`);
+    }
+});
+
+client.login(token);
