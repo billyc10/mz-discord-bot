e72d57118989d866bac1f0a30a4cc78b476780d2
Billy Chen <billychen10@hotmail.com>
2020-09-19 21:39:48 +1000

    removed gay check

--- src/index.js
+++ src/index.js
@@ -4,15 +4,11 @@ const client = new Discord.Client();
 const {users, token, prefix} = require('./config').config;
 const messageService = require('./messageService');
 
-var countdown = 6;
-
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-    console.log(msg.content);
-
     if (msg.author.bot) return;
 
     const message = msg.content.toLowerCase();
@@ -66,38 +62,6 @@ client.on('message', msg => {
             .setTimestamp()
 
         msg.channel.send(ripText);
-    }
-
-    // If xavier messages, test if he's gay
-    if (msg.author == users.xavier) {
-        const initiateCountdown = () => {
-            if (countdown == 0) {
-                msg.channel.send("HA! GOTIMMMMMM");
-                countdown = 6;
-                return;
-            }
-
-            if (countdown == 6) {
-                msg.channel.send("Ooft, saved by the bell");
-                return;
-            }
-
-            msg.channel.send(countdown);
-            countdown -= 1;
-            setTimeout(initiateCountdown, 2000);
-        }
-        
-        // Send the message at a random interval between 0 - 60 seconds after xavier is active in channel
-        // If xavier responds reset the countdown and clear the rest of the messages.
-        if (countdown != 6) {
-            countdown = 6;
-        } else if (countdown == 6) {
-            countdown = 5;
-            setTimeout(() => {
-                msg.channel.send("Hey Xavier, if you don't respond in 5 seconds, you're gay");
-                setTimeout(initiateCountdown, 1000);
-            }, 20000*Math.random());
-        }
     }
 });
 
